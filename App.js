d88a16d55df0b2a6cd890e3741877dd3194f5b04
Muhammad Talha <muhammadtalha517@gmail.com>
2021-12-30 20:58:09 +0500

    Cleared the bugs and created new component{incomplete} for background....

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
-import {RoundedCardWithAvatar, RoundedServiceCard} from "./components/MyComponents"
+import {RoundedCardWithAvatar, RoundedServiceCard, Background} from "./components/MyComponents"
 import { Avatar } from 'react-native-paper';
 // You can import from local files
-import AssetExample from './components/AssetExample';
+
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-        <RoundedServiceCard title="affaf"/>      
+    <View style={{backgroundColor:'red'}}>
+        {/* <Background/> */}
+      <Text>aaa</Text>
       </View>
   );
 }
@@ -22,6 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "100%",
+    backgroundColor:"red",
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
